74276f3949ce02bc485f6e40a6be9474f44e4fba
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2021-03-05 09:04:49 +1000

    add ugly template to test generation

--- demo/30-template-document.ts
+++ demo/30-template-document.ts
@@ -1,10 +1,11 @@
 // Example on how to use a template document
 // Import from 'docx' rather than '../build' if you install from npm
 import * as fs from "fs";
-import { Document, ImportDotx, Packer, Paragraph } from "../build";
+import { Document, ImportDotx, Packer, PageBreak, Paragraph } from "../build";
 
 const importDotx = new ImportDotx();
-const filePath = "./demo/dotx/template.dotx";
+// const filePath = "./demo/dotx/template.dotx";
+const filePath = "./demo/dotx/TestTemplate.dotx";
 
 fs.readFile(filePath, (err, data) => {
     if (err) {
@@ -32,7 +33,13 @@ fs.readFile(filePath, (err, data) => {
                     {
                         headers: templateHeaders,
                         footers: templateFooters,
-                        children: [new Paragraph("Hello page 2")],
+                        children: [
+                            new Paragraph("Hello page 2"),
+                            new PageBreak(),
+                            new Paragraph("Hello page 3"),
+                            new PageBreak(),
+                            new Paragraph("Hello page 4"),
+                        ],
                     }
                 ],
             },
@@ -42,7 +49,8 @@ fs.readFile(filePath, (err, data) => {
         );
 
         Packer.toBuffer(doc).then((buffer) => {
-            fs.writeFileSync("My Document.docx", buffer);
+            // fs.writeFileSync("My Document.docx", buffer);
+            fs.writeFileSync("My Ugly Document.docx", buffer);
         });
     });
 });
