8fbcbdb26486c760dc43649dec5ed4df3d7bd326
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2021-04-09 17:25:27 +1000

    refactor(template): update template test to use custom template

--- demo/30-template-document.ts
+++ demo/30-template-document.ts
@@ -1,11 +1,13 @@
+/* tslint:disable */
 // Example on how to use a template document
 // Import from 'docx' rather than '../build' if you install from npm
 import * as fs from "fs";
-import { Document, ImportDotx, Packer, PageBreak, Paragraph } from "../build";
+import { Document, ImportDotx, Packer, Paragraph } from "../build";
+import { PageBreak, TextRun } from "../build";
 
 const importDotx = new ImportDotx();
 // const filePath = "./demo/dotx/template.dotx";
-const filePath = "./demo/dotx/TestTemplate.dotx";
+const filePath = "./__exclude/dotx/TestTemplate.dotx";
 
 fs.readFile(filePath, (err, data) => {
     if (err) {
@@ -14,6 +16,9 @@ fs.readFile(filePath, (err, data) => {
 
     importDotx.extract(data).then((templateDocument) => {
         // NOTE: this `Document` class is really the File class from `/src/file/file.ts`
+        /**
+         * CFH: review the `IPropertiesOptions`
+         */
 
         // Map headers to known keys (default, first, even)
         const templateHeaders = ImportDotx.getHeaders(templateDocument);
@@ -28,17 +33,66 @@ fs.readFile(filePath, (err, data) => {
                         properties: {
                             titlePage: templateDocument.titlePageIsDefined,
                         },
-                        children: [new Paragraph("Hello World")],
+                        children: [
+                            new Paragraph({
+                                children: [
+                                    new TextRun("Hello World - page 1"),
+                                    new PageBreak(),
+                                ],
+                            }),
+                            new Paragraph({
+                                children: [
+                                    new TextRun("Hello World - page 2"),
+                                    new PageBreak(),
+                                ],
+                            }),
+                            new Paragraph({
+                                children: [
+                                    new TextRun("Hello World - page 3"),
+                                    new PageBreak(),
+                                ],
+                            }),
+                            new Paragraph({
+                                children: [
+                                    new TextRun("Hello World - page 4"),
+                                    new PageBreak(),
+                                ],
+                            }),
+                        ],
                     },
                     {
-                        headers: templateHeaders,
-                        footers: templateFooters,
+                        // headers: templateHeaders,
+                        // footers: templateFooters,
                         children: [
-                            new Paragraph("Hello page 2"),
-                            new PageBreak(),
-                            new Paragraph("Hello page 3"),
-                            new PageBreak(),
+                            new Paragraph({
+                                children: [
+                                    new TextRun("(SECTION 2) Hello World"),
+                                    new PageBreak(),
+                                ],
+                            }),
+                            new Paragraph({
+                                children: [
+                                    new TextRun("Hello page 3"),
+                                    new PageBreak(),
+                                ],
+                            }),
                             new Paragraph("Hello page 4"),
+                        ],
+                    },
+                    {
+                        // headers: templateHeaders,
+                        // headers: {
+                        //      // For an empty Header
+                        //     default: new Header(),
+                        // },
+                        // footers: templateFooters,
+                        children: [
+                            new Paragraph({
+                                children: [
+                                    new TextRun("(SECTION 3) Hello World"),
+                                    new PageBreak(),
+                                ],
+                            }),
                         ],
                     }
                 ],
@@ -50,7 +104,7 @@ fs.readFile(filePath, (err, data) => {
 
         Packer.toBuffer(doc).then((buffer) => {
             // fs.writeFileSync("My Document.docx", buffer);
-            fs.writeFileSync("My Ugly Document.docx", buffer);
+            fs.writeFileSync("./__exclude/output/My Ugly Document3.docx", buffer);
         });
     });
 });
