7fa3d1d1490b13f8b69b838783c91658703e48ae
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2021-04-11 12:31:45 +1000

    refactor(dotx): fiddle with dotx template test

--- demo/30-template-document.ts
+++ demo/30-template-document.ts
@@ -2,19 +2,19 @@
 // Example on how to use a template document
 // Import from 'docx' rather than '../build' if you install from npm
 import * as fs from "fs";
-import { Document, ImportDotx, Packer, Paragraph } from "../build";
-import { PageBreak, TextRun } from "../build";
+import { Document, ImportDotx, Packer, Paragraph, IDocumentTemplate } from "../build";
+import { TableOfContents, PageBreak, TextRun, Header, Footer } from "../build";
 
 const importDotx = new ImportDotx();
 // const filePath = "./demo/dotx/template.dotx";
-const filePath = "./__exclude/dotx/TestTemplate.dotx";
+const filePath = "./__exclude/dotx/2021-04-11-DemoTemplate.dotx";
 
 fs.readFile(filePath, (err, data) => {
     if (err) {
         throw new Error(`Failed to read file ${filePath}.`);
     }
 
-    importDotx.extract(data).then((templateDocument) => {
+    importDotx.extract(data).then((templateDocument: IDocumentTemplate) => {
         // NOTE: this `Document` class is really the File class from `/src/file/file.ts`
         /**
          * CFH: review the `IPropertiesOptions`
@@ -24,16 +24,31 @@ fs.readFile(filePath, (err, data) => {
         const templateHeaders = ImportDotx.getHeaders(templateDocument);
         const templateFooters = ImportDotx.getFooters(templateDocument);
 
+        const toc = new TableOfContents("Summary", {
+            hyperlink: true,
+            headingStyleRange: "1-5",
+            // stylesWithLevels: [new StyleLevel("MySpectacularStyle", 1)],
+        });
+        // doc.addTableOfContents(toc);
+
         const doc = new Document(
             {
+                evenAndOddHeaderAndFooters: ('even' in templateHeaders),
                 sections: [
                     {
                         headers: templateHeaders,
                         footers: templateFooters,
                         properties: {
                             titlePage: templateDocument.titlePageIsDefined,
+                            // titlePage: false,
                         },
                         children: [
+                            // toc,
+                            new Paragraph({
+                                children: [
+                                    new PageBreak(),
+                                ],
+                            }),
                             new Paragraph({
                                 children: [
                                     new TextRun("Hello World - page 1"),
@@ -61,6 +76,12 @@ fs.readFile(filePath, (err, data) => {
                         ],
                     },
                     {
+                        // headers: {
+                        //     default: new Header(),
+                        // },
+                        // footers: {
+                        //     default: new Footer(),
+                        // },
                         // headers: templateHeaders,
                         // footers: templateFooters,
                         children: [
@@ -102,7 +123,10 @@ fs.readFile(filePath, (err, data) => {
             },
         );
 
-        Packer.toBuffer(doc).then((buffer) => {
+        doc.Document.View.getTablesOfContents();
+        doc.verifyUpdateFields();
+
+        Packer.toBuffer(doc as any).then((buffer) => {
             // fs.writeFileSync("My Document.docx", buffer);
             fs.writeFileSync("./__exclude/output/My Ugly Document3.docx", buffer);
         });
